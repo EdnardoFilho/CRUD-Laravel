bc71dcecd8d964e08a3e0a39214867cc9b6b0b3d
EdnardoFilho <ednardofilho49@gmail.com>
2021-06-03 00:21:47 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,49 @@
+<?php
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'as' => 'api.'
+], function(){
+
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::group([
+        'prefix' => 'posts',
+        'as' => 'posts.'
+    ], function(){
+        Route::get('/', [PostController::class, 'index'])->name('index');
+        Route::post('/', [PostController::class, 'store'])->name('store');
+        Route::get('/{id}', [PostController::class, 'show'])->name('show');
+        Route::put('/{id}', [PostController::class, 'update'])->name('update');
+        Route::delete('/{id}', [PostController::class, 'destroy'])->name('destroy');
+    });
+
+    Route::group([
+        'prefix' => 'users',
+        'as' => 'users.'
+    ], function(){
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('/{id}', [UserController::class, 'show'])->name('show');
+        Route::put('/{id}', [UserController::class, 'update'])->name('update');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
+    });
+
+});
